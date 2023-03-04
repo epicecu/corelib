888c9fc673169eb859168b555f3b4e3a30a142fa
David Cedar <david@epicecu.com>
2023-03-04 23:23:21 +1030

    Wip adding proto files to env var; npm install cfg

---
+++ protobuf/add_files_env.py
@@ -0,0 +1,12 @@
+import os
+Import("env")
+
+protobuf_folder = os.path.join(os.getcwd(), '.')
+
+env["custom_nanopb_protos"] = env["custom_nanopb_protos"] +  f" +<{protobuf_folder}/*.proto>"
+env["custom_nanopb_options"] = env["custom_nanopb_options"] + f" --error-on-unmatched"
+
+global_env = DefaultEnvironment()
+
+global_env["custom_nanopb_protos"] = global_env["custom_nanopb_protos"] +  f" +<{protobuf_folder}/*.proto>"
+global_env["custom_nanopb_options"] = global_env["custom_nanopb_options"] + f" --error-on-unmatched"
