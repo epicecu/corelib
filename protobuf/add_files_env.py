299a699945d46486ea98b788b38ece669fbdf615
David Cedar <david@epicecu.com>
2023-03-04 23:28:30 +1030

    Wip python script test 1

--- protobuf/add_files_env.py
+++ protobuf/add_files_env.py
@@ -3,8 +3,8 @@ Import("env")
 
 protobuf_folder = os.path.join(os.getcwd(), '.')
 
-env["custom_nanopb_protos"] = env["custom_nanopb_protos"] +  f" +<{protobuf_folder}/*.proto>"
-env["custom_nanopb_options"] = env["custom_nanopb_options"] + f" --error-on-unmatched"
+# env["custom_nanopb_protos"] = env["custom_nanopb_protos"] +  f" +<{protobuf_folder}/*.proto>"
+# env["custom_nanopb_options"] = env["custom_nanopb_options"] + f" --error-on-unmatched"
 
 global_env = DefaultEnvironment()
 
