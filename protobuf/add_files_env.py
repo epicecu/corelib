f1d59e17157c2c695e0223d028a60ddb94212a3b
David Cedar <david@epicecu.com>
2023-03-04 23:40:19 +1030

    Test

--- protobuf/add_files_env.py
+++ protobuf/add_files_env.py
@@ -3,12 +3,15 @@ Import("env")
 
 protobuf_folder = os.path.join(os.getcwd(), '.')
 
-global_env = DefaultEnvironment()
+env["custom_nanopb_protos"] = f" +<{protobuf_folder}/*.proto>"
+env["custom_nanopb_options"] = f" --error-on-unmatched"
 
-global_env.Append(
-    custom_nanopb_protos=f"+<{protobuf_folder}/*.proto>"
-)
+# global_env = DefaultEnvironment()
 
-global_env.Append(
-    custom_nanopb_options=f"--error-on-unmatched"
-)
+# global_env.Append(
+#     custom_nanopb_protos=f"+<{protobuf_folder}/*.proto>"
+# )
+
+# global_env.Append(
+#     custom_nanopb_options=f"--error-on-unmatched"
+# )
